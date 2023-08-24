2ccf42c27ff4f30120be4bb12ed74aa50d54b289
sameer nikam <sameernikam01112001@gmail.com>
2023-08-24 19:12:06 +0530

    String Method in Javascript

---
+++ script.js
@@ -0,0 +1,94 @@
+//string method
+//1.length
+// let str = "my name is sameer shriram nikam";
+// console.log(str.length);//31
+
+//2.trim
+// let str = "my name is sameer shriram nikam";
+// console.log(str.trim());//my name is sameer shriram nikam
+
+//3.includes
+// let str = "maxotag technology";
+// let str1 = "maxotag";
+// console.log(str.includes(str1)); //true
+// let str2 = "test";
+// console.log(str.includes(str2));//false
+
+//4.indexof
+// let str = "maxotag technology";
+// console.log(str.indexOf("maxotag")); //0
+// console.log(str.indexOf("sameer"));//-1
+
+//5.toUpperCase
+// let str = "maxotag technology";
+// console.log(str.toUpperCase());//MAXOTAG TECHNOLOGY
+
+//6.toLowerCase
+// let str = "MAXOTAG TECHNOLOGY";
+// console.log(str.toLowerCase());//maxotag technology
+
+//7.replace
+// let str = "MAXOTAG TECHNOLOGY";
+// console.log(str.replace("MAXOTAG", "Maxo"));//Maxo TECHNOLOGY
+
+//8.slice
+// let str = "maxotag technology";
+// console.log(str.slice(2, 5));//xot
+
+//9.split
+// let str = "maxotag technology";
+// console.log(str.split(" "));//['maxotag', 'technology']
+
+//10.repeat
+// let str = "maxotag technology";
+// console.log(str.repeat(5));//maxotag technologymaxotag technologymaxotag technologymaxotag technologymaxotag technology
+
+//11.match
+// let str = "maxotag technology";
+// console.log(str.match("maxotag"));//["maxotag"]
+
+//12.charAt
+// let str = "maxotag technology";
+// console.log(str.charAt(6));//g
+
+//13.charCodeAt
+// let str = "maxotag technology";
+// console.log(str.charCodeAt(1));//97
+
+//14.concat
+// let str = "maxotag technology";
+// let str1 = "Welcome to ";
+// console.log(str1.concat(str));//Welcome to maxotag technology
+
+//15.search
+// let str = "Welcome to maxotag technology";
+// console.log(str.search("maxotag"));//11
+
+//16.subString
+// let str = "maxotag technology";
+// console.log(str.substring(3, 6));//ota
+
+//17.substr
+// let str = "maxotag technology";
+// console.log(str.substr(2, 5));//xotag
+
+//18.startsWith
+// let str = "maxotag technology";
+// console.log(str.startsWith("technology"));//false
+
+//19.endsWith
+// let str = "maxotag technology";
+// console.log(str.endsWith("technology"));//true
+
+//20.lastIndexOf
+// let str = "maxotag technology";
+// console.log(str.lastIndexOf("maxotag technology"));//0
+
+//21.fromCharCode
+// let myString = "";
+// for (let charCode = 65; charCode <= 90; charCode++) {
+//   myString += String.fromCharCode(charCode);
+// }
+// console.log(myString);//a to z
+
+
